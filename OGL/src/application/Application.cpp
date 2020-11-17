a4640590f858d7cbf8e9599ee2d94cea879c2f0f
Vlad Dogadaev <vmdogad@gmail.com>
2020-11-17 23:53:54 +0100

    [WIP] Shader abstraction

--- OGL/src/application/Application.cpp
+++ OGL/src/application/Application.cpp
@@ -2,97 +2,11 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <sstream>
 
 #include "../buffer/vertex/VertexBuffer.h"
 #include "../buffer/vertex/VertexArray.h"
 #include "../buffer/index/IndexBuffer.h"
-
-struct ShaderProgram
-{
-    std::string VertexShader;
-    std::string FragmentShader;
-};
-
-static ShaderProgram ParseShader(const std::string& filepath)
-{
-    enum class ShaderType
-    {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
-
-    std::ifstream stream(filepath);
-    std::stringstream s_stream[2];
-    std::string line;
-
-    ShaderType currentShaderType = ShaderType::NONE;
-
-    while (getline(stream, line))
-    {
-        if (line.find("#shader") != std::string::npos)
-        {
-            if (line.find("vertex") != std::string::npos)
-                currentShaderType = ShaderType::VERTEX;
-            else if (line.find("fragment") != std::string::npos)
-                currentShaderType = ShaderType::FRAGMENT;
-        }
-        else
-        {
-            s_stream[(int) currentShaderType] << line << "\n";
-        }
-    }
-
-    stream.close();
-
-    return { s_stream[(int) ShaderType::VERTEX].str(), s_stream[(int) ShaderType::FRAGMENT].str() };
-}
-
-static uint32_t compileShader(uint32_t type, const std::string& source)
-{
-    uint32_t shaderId = glCreateShader(type);
-    const char* shaderSource = source.c_str();
-
-    glShaderSource(shaderId, 1, &shaderSource, nullptr);
-    glCompileShader(shaderId);
-
-    int result;
-    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &result);
-
-    if (result == GL_FALSE)
-    {
-        int length;
-        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &length);
-        
-        char* errorMessage = (char*) _malloca(length * sizeof(char));
-        glGetShaderInfoLog(shaderId,length, &length, errorMessage);
-
-        std::cout << "Could not compile a shader!" << "\n" << errorMessage << std::endl;
-
-        glDeleteShader(shaderId);
-        return 0;
-    }
-
-    return shaderId;
-}
-
-static uint32_t createShader(const std::string& vertexShader, const std::string& fragmentShader)
-{
-    uint32_t program = glCreateProgram();
-    uint32_t vertexShaderId = compileShader(GL_VERTEX_SHADER, vertexShader);
-    uint32_t fragmentShaderId = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-    glAttachShader(program, vertexShaderId);
-    glAttachShader(program, fragmentShaderId);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vertexShaderId);
-    glDeleteShader(fragmentShaderId);
-
-    return program;
-}
 
 static void incrementColorValue(float_t &increment, float_t &color)
 {
@@ -125,7 +39,6 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
---
+++ OGL/src/shader/Shader.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <GL/glew.h>
+
+struct ShaderProgram
+{
+	std::string VertexShader;
+	std::string FragmentShader;
+};
+
+class Shader
+{
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void bind() const;
+	void unbind() const;
+
+	void setUniform4f(const std::string& name, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+
+private:
+	uint32_t m_rendererId;
+
+	uint32_t compileShader(uint32_t type, const std::string& source);
+	ShaderProgram parseShader(const std::string& filepath);
+	uint32_t getUniformLocation(const std::string& name);
+};
