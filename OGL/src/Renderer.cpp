7c2b84a168ed8db812ab04970901e4c46d67280e
Vlad Dogadaev <vmdogad@gmail.com>
2020-11-18 20:27:27 +0100

    Moved out all opengl related code to a renderer

--- OGL/src/Renderer.cpp
+++ OGL/src/Renderer.cpp
@@ -1,6 +1,60 @@
 #include "Renderer.h"
 
-#include <GL/glew.h>
+#include<iostream>
+#include<string>
+
+void GLAPIENTRY onGLError(GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar* message,
+    const void* userParam)
+{
+    std::cout <<
+        "ERROR: " << type <<
+        "\nMessage: " << message <<
+        "\n" << std::endl;
+    //__debugbreak();
+}
+
+Renderer::Renderer(const std::string& title, const uint32_t width, const uint32_t height)
+{
+    /* Initialize the library */
+    if (!glfwInit()) __debugbreak();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    m_window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+    if (!m_window)
+    {
+        glfwTerminate();
+        __debugbreak();
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(m_window);
+
+    /* Initialize GLEW */
+    glewInit();
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(onGLError, 0);
+
+    std::cout << glGetString(GL_VERSION) << "\n" << glGetString(GL_RENDERER) << "\n" << std::endl;
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+/* Loop until the user closes the window */
+bool Renderer::isRunning() const
+{
+    return !glfwWindowShouldClose(m_window);
+}
 
 void Renderer::clear() const
 {
@@ -13,4 +67,9 @@ void Renderer::draw(const VertexArray& vertexArray, const IndexBuffer& indexBuff
 
 	glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_INT, nullptr);
 
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
 }
