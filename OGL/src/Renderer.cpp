9a8d5fa4910b4a571679e3fb655e6399b592fb01
Vlad Dogadaev <vmdogad@gmail.com>
2020-11-18 15:18:07 +0100

    Implemented basic renderer

---
+++ OGL/src/Renderer.cpp
@@ -0,0 +1,15 @@
+#include "Renderer.h"
+
+#include <GL/glew.h>
+
+void Renderer::clear() const
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Renderer::draw(const VertexArray& vertexArray, const IndexBuffer& indexBuffer) const
+{
+	indexBuffer.bind();
+	glDrawElements(GL_TRIANGLES, indexBuffer.getCount(), GL_UNSIGNED_INT, nullptr);
+
+}
