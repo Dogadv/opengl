0fbcd664a52b2253c027de3c093e8189536ddf03
Vlad Dogadaev <vmdogad@gmail.com>
2021-02-12 23:57:31 +0100

    Dynamic geometry

--- OGL/src/application/Application.cpp
+++ OGL/src/application/Application.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <array>
 
 #include "../entity/Vertex.h"
 
@@ -17,6 +18,18 @@
 
 const char* glsl_version = "#version 130";
 
+static std::array<Vertex, 4> buildQuad(const glm::vec2& position, const GLuint textureIndex)
+{
+    GLfloat size = 100.0f;
+
+    Vertex v0 = { { position.x,        position.y        }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f ,0.0f }, textureIndex };
+    Vertex v1 = { { position.x + size, position.y        }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 0.0f }, textureIndex };
+    Vertex v2 = { { position.x + size, position.y + size }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f }, textureIndex };
+    Vertex v3 = { { position.x,        position.y + size }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 1.0f }, textureIndex };
+
+    return { v0, v1, v2, v3 };
+}
+
 int main(void)
 {
     const uint32_t width = 1280;
@@ -31,20 +44,10 @@ int main(void)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    Vertex verticies[2 * 4] = {
-        {{-100.0f, -100.0f},  {0.0f, 1.0f, 0.0f, 1.0f},   {0.0f ,0.0f},    0},
-        {{ 100.0f, -100.0f},  {0.0f, 1.0f, 0.0f, 1.0f},   {0.0f, 0.0f},    0},
-        {{ 100.0f,  100.0f},  {0.0f, 1.0f, 0.0f, 1.0f},   {0.0f, 1.0f},    0},
-        {{-100.0f,  100.0f},  {0.0f, 1.0f, 0.0f, 1.0f},   {0.0f, 1.0f},    0},
-
-        {{ 100.0f,  100.0f},  {1.0f, 0.0f, 0.0f, 1.0f},   {0.0f ,0.0f},    1},
-        {{ 300.0f,  100.0f},  {1.0f, 0.0f, 0.0f, 1.0f},   {0.0f, 0.0f},    1},
-        {{ 300.0f,  300.0f},  {1.0f, 0.0f, 0.0f, 1.0f},   {0.0f, 1.0f},    1},
-        {{ 100.0f,  300.0f},  {1.0f, 0.0f, 0.0f, 1.0f},   {0.0f, 1.0f},    1},
-    };
+    const int verteciesCount = 8;
 
     VertexArray vertexArray;
-    VertexBuffer vertexBuffer(sizeof(verticies));
+    VertexBuffer vertexBuffer(sizeof(Vertex) * 8);
     VertexBufferLayout vertexBufferLayout;
 
     vertexBufferLayout.push<GLfloat>(VERTEX_POSITION_COMPONENT_COUNT);
@@ -82,11 +85,24 @@ int main(void)
     glm::vec3 cameraTranslation(500.0f, 250.0f, 0.0);
     glm::vec3 modelTranslation(0.0f, 0.0f, 0.0f);
 
+    glm::vec2 q0Translation(0.0f, 0.0f);
+    glm::vec2 q1Translation(100.0f, 100.0f);
+
     GLfloat zoom = 1.0f;
+
 
     while (renderer.isRunning())
     {
+        auto q0 = buildQuad(q0Translation, 0);
+        auto q1 = buildQuad(q1Translation, 1);
+
+        Vertex verticies[8];
+
+        memcpy(verticies, q0.data(), q0.size() * sizeof(Vertex));
+        memcpy(verticies + q0.size(), q1.data(), q1.size() * sizeof(Vertex));
+
         vertexBuffer.bind(verticies, sizeof(verticies));
+
         renderer.clear();
 
         ImGui_ImplOpenGL3_NewFrame();
@@ -94,6 +110,8 @@ int main(void)
         ImGui::NewFrame();
 
         shader.bind();
+        oglTexture.bind();
+        cobblestoneTexture.bind();
         shader.setUniformMat4f("u_mvp", renderer.getMVPMatrix(modelTranslation, cameraTranslation, zoom));
 
         renderer.draw(vertexArray, indexBuffer);
@@ -102,6 +120,8 @@ int main(void)
             ImGui::Begin("params");
             ImGui::SliderFloat2("camera position", &cameraTranslation.x, 0, (float)width, "%g", 0);
             ImGui::SliderFloat("camera zoom", &zoom, .25f, 2.5f, "%g", 0);
+            ImGui::SliderFloat2("object 1 position", &q0Translation.x, 0, (float)width, "%g", 0);
+            ImGui::SliderFloat2("object 2 position", &q1Translation.x, 0, (float)width, "%g", 0);
             ImGui::End();
         }
 
@@ -111,9 +131,9 @@ int main(void)
         renderer.update();
     }
 
-    //ImGui_ImplOpenGL3_Shutdown();
-    //ImGui_ImplGlfw_Shutdown();
-    //ImGui::DestroyContext();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
